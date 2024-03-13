7ce42ed6812dc9a6fee4f102d712e6e1eb42dbce
preethampatelare <areshashipreetham@gmail.com>
2024-03-14 00:48:11 +0530

    small changes

---
+++ app.js
@@ -0,0 +1,25 @@
+// let input = prompt("Enter your name: ");
+// console.log("heartly welcome ", input);
+
+
+// String methods
+
+// let str = "      shashi preetham      ";
+// console.log(input.toUpperCase()); OutPut: SHASHI PREETHAM
+//console.log(input.toLowerCase()); OutPut: shashi preetham
+// console.log(input.indexOf("shi"));
+//Method Chaining
+//console.log(input.trim().toUpperCase());
+//slicing method
+// console.log(input.slice(-4));
+//replace method
+//console.log(input.replace("bubu","Shivajyothi"));
+//console.log(input.replace("s","p"));
+//repeat method
+//console.log(input.repeat(20));
+//Practice Questions
+//console.log(input.slice(4,9));
+// console.log(input.indexOf("na"));
+// console.log(input.replace("Apna","Our"));
+let array = [1,5,6,8,3,6,9,0];
+console.log(array);
